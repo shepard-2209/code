943298b917bb5f28bed793c60b4ff4022325de41
shepard-2209 <it-gimenes@hotmail.com>
2019-12-18 14:36:20 -0300

    fundamentos completos/ controles

---
+++ Programação WEB/exercicios-js/fundamentos/erro.js
@@ -0,0 +1,25 @@
+function tratarErroELancar(erro) {
+    // throw new Error('...')
+    // throw 10
+    // throw true
+    // throw 'mensagem'
+    throw {
+        nome: erro.name,
+        ms: erro.message,
+        date: new Date
+    }
+}
+
+function imprimirNomeGritado(obj) {
+    try {
+        console.log(obj.name.toUpperCase() + '!!!')
+    } catch (e) {
+        tratarErroELancar(e)
+    } finally {
+        console.log('Final')
+    }
+
+}
+
+const obj = { name: 'Roberto' }
+imprimirNomeGritado(obj)
