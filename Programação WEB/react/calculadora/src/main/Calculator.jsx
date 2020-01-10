746b209aebcad73d3beafd2b76cef10890dd2f99
shepard-2209 <it-gimenes@hotmail.com>
2020-01-10 12:16:04 -0200

    projeto cadastro-usuario em REACT/frontend

--- Programação WEB/react/calculadora/src/main/Calculator.jsx
+++ Programação WEB/react/calculadora/src/main/Calculator.jsx
@@ -36,8 +36,22 @@ export default class Calculator extends Component {
             const equals = operation === '='
             const currentOperation = this.state.operation
 
-            const values = {...this.state.values}
-            values[0] = eval(`${values[0]}`)
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch (e) {
+                values[0] = this.state.values[0]
+            }
+            
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
         }
     }
 
