c054809d58d1567800f94e8c447374f7a234cbb2
shepard-2209 <it-gimenes@hotmail.com>
2020-01-10 14:32:47 -0300

    backend cadastro de usuário / rotas

--- Programação WEB/react/crud/frontend/src/components/template/Logo.jsx
+++ Programação WEB/react/crud/frontend/src/components/template/Logo.jsx
@@ -1,7 +1,12 @@
 import './Logo.css'
+import logo from '../../assets/images/slack.svg'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default props =>
     <aside className="logo">
-        Logo
+        <Link to="/" className="logo">
+            <img src={logo} alt="logo" />
+        </Link>
+
     </aside>
